0f80155ab8b2a72686b43e60fea0b7f20c1d026e
majian1234554321 <99571202@qq.com>
2018-10-25 08:59:54 +0800

    基础模块的抽取

--- app/src/main/java/com/yjhh/ppwcustomer/bean/LoginBean.kt
+++
@@ -1,8 +0,0 @@
-package com.yjhh.ppwcustomer.bean
-
-import com.yjhh.ppwcustomer.api.BaseResponse
-
-
-data class LoginBean( var name: String,var data:Log2): BaseResponse<Log2>()
-
-data class Log2(var name: String)
--- app/src/main/java/com/yjhh/ppwcustomer/view/LoginView.kt
+++
@@ -1,9 +0,0 @@
-package com.yjhh.ppwcustomer.view
-
-import com.yjhh.ppwcustomer.bean.LoginBean
-
-interface LoginView {
-    abstract fun onSuccess(result: LoginBean?)
-
-    abstract fun onFault(errorMsg: String?)
-}
